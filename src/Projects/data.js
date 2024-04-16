601ead3b0d713ec7d819498407dfbbf1eab02b80
sherif <o.d.alsherif@gmail.com>
2024-04-16 05:03:15 +0100

    UPDATE

--- src/Projects/data.js
+++ src/Projects/data.js
@@ -1,115 +1,129 @@
-import shoklin from './portfolio_img/05.11.2022_03.51.50_REC.png';
-import apple from './portfolio_img/AppleStore.png';
-import university from './portfolio_img/alicia.png';
-import vividioz from './portfolio_img/vivioz.PNG';
-import country from './portfolio_img/countriesqq.PNG';
-import vatdoc from '../assets/vatdoc.PNG';
-import verifier from '../assets/verifier.PNG';
-import itemify from '../assets/itemify.PNG';
-import appraiseMe from '../assets/appraiseme.PNG';
-import adserve from '../assets/adserve.PNG';
-import wardboard from '../assets/wardboard.jpg';
-import approveit from '../assets/approve-it (2).PNG';
-import unshelled from './portfolio_img/unshelled.PNG';
-import page from './portfolio_img/page.PNG';
-import hq from './portfolio_img/moneyHq.jpeg';
-import examinator from '../assets/examinator.jpeg';
+import shoklin from "./portfolio_img/05.11.2022_03.51.50_REC.png";
+import apple from "./portfolio_img/AppleStore.png";
+import university from "./portfolio_img/alicia.png";
+import vividioz from "./portfolio_img/vivioz.PNG";
+import country from "./portfolio_img/countriesqq.PNG";
+import vatdoc from "../assets/vatdoc.PNG";
+import verifier from "../assets/verifier.PNG";
+import itemify from "../assets/itemify.PNG";
+import appraiseMe from "../assets/appraiseme.PNG";
+import adserve from "../assets/adserve.PNG";
+import wardboard from "../assets/wardboard.jpg";
+import approveit from "../assets/approve-it (2).PNG";
+import unshelled from "./portfolio_img/unshelled.PNG";
+import page from "./portfolio_img/page.PNG";
+import hq from "./portfolio_img/moneyHq.jpeg";
+import examinator from "../assets/examinator.jpeg";
+import streetFood from "../assets/streetfood.jpeg";
+import accountable from "../assets/accountable.jpeg";
 
 export const data = [
-    {
-        title: 'MoneyHQ',
-        demo: 'https://dev.moneyhq.io/',
-        github: 'n/a',
-        img: hq,
-    },
-     {
-        title: 'Wardboard(vpn needed)',
-        demo: 'https://wardbord.com/',
-        github: 'N/A',
-        img: wardboard
-    },
-     {
-        title: 'Examinator',
-        demo: "https://examinator.netlify.app/",
-        github: 'N/A',
-        img: examinator
-    },
-    {
-        title: 'Vatdoc',
-        demo: 'https://vatdoc.netlify.app/',
-        github: 'https://github.com/Lawdshev/Vatdoc',
-        img: vatdoc,
-    },
-    {
-        title: 'Verifier',
-        demo: 'https://everifyer.netlify.app/',
-        github: 'https://github.com/Lawdshev/Everifyer',
-        img: verifier,
-    },
-    {
-        title: 'Itemify',
-        demo: 'https://itemify.netlify.app/',
-        github: 'https://github.com/Lawdshev/Itemify',
-        img: itemify,
-    },
-    {
-        title: 'Shoklin',
-        demo: 'https://shoklin.onrender.com',
-        github: 'https://github.com/Lawdshev/shoklin',
-        img: shoklin,
-    },
-    {
-        title: 'Apple',
-        demo: 'https://majestic-gelato-78453c.netlify.app/',
-        github: 'https://github.com/Lawdshev/Apple',
-        img: apple,
-    },
-    {
-        title: 'Alicia University',
-        demo: 'https://aliciauniversity.netlify.app',
-        github: 'https://github.com/Lawdshev/Alicia-University',
-        img: university,
-    },
-    {
-        title: 'Vividioz',
-        demo: 'https://vividioz.onrender.com/',
-        github: 'https://github.com/rash1234567/NEWVIVIDIO',
-        img: vividioz,
-    },
-    {
-        title: 'Adserve',
-        demo: 'https://adserve.netlify.app/',
-        github: 'https://github.com/Lawdshev/adserve',
-        img: adserve,
-    },
-    {
-        title: 'AppraiseMe',
-        demo: 'https://appraiseMe.netlify.app/',
-        github: 'https://github.com/Lawdshev/appraise',
-        img: appraiseMe,
-    },
-    {
-        title: 'CountriesQQ',
-        demo: 'https://countriesQQ.onrender.com',
-        github: 'https://github.com/Lawdshev/vatebra_test',
-        img: country,
-    },
-    {
-        title: 'unshelled',
-        demo: 'https://unshelled-client.onrender.com/',
-        github: 'https://github.com/Lawdshev/unshelled-frontend',
-        img: unshelled
-    },
-    {
-        title: 'P&G ',
-        demo: 'https://super-pavlova-0ce678.netlify.app/',
-        github: 'https://github.com/Lawdshev/page',
-        img: page
-    },
-    {
-        title: 'Approve-it ',
-        demo: 'https://approve-it.onrender.com/',
-        github: 'https://github.com/Lawdshev/ApproveIT',
-        img: approveit
-    }
-]
+  {
+    title: "MoneyHQ",
+    demo: "https://dev.moneyhq.io/",
+    github: "n/a",
+    img: hq,
+  },
+  {
+    title: "Wardboard",
+    demo: "https://wardbord.com/",
+    github: "N/A",
+    img: wardboard,
+  },
+  {
+    title: "Street Food",
+    demo: "https://street-food-eight.vercel.app/",
+    github: "N/A",
+    img: streetFood,
+  },
+  {
+    title: "Accountable",
+    demo: "https://accountable-sepia.vercel.app/",
+    github: "N/A",
+    img: accountable,
+  },
+  {
+    title: "P&G ",
+    demo: "https://super-pavlova-0ce678.netlify.app/",
+    github: "N/A",
+    img: page,
+  },
+  {
+    title: "Approve-it ",
+    demo: "https://approve-it.netlify.app/",
+    github: "N/A",
+    img: approveit,
+  },
+  {
+    title: "Examinator",
+    demo: "https://examinator.netlify.app/",
+    github: "N/A",
+    img: examinator,
+  },
+  {
+    title: "Vatdoc",
+    demo: "https://vatdoc.netlify.app/",
+    github: "https://github.com/Lawdshev/Vatdoc",
+    img: vatdoc,
+  },
+  {
+    title: "Verifier",
+    demo: "https://everifyer.netlify.app/",
+    github: "https://github.com/Lawdshev/Everifyer",
+    img: verifier,
+  },
+  {
+    title: "Itemify",
+    demo: "https://itemify.netlify.app/",
+    github: "https://github.com/Lawdshev/Itemify",
+    img: itemify,
+  },
+  {
+    title: "Shoklin",
+    demo: "https://shoklin.onrender.com",
+    github: "https://github.com/Lawdshev/shoklin",
+    img: shoklin,
+  },
+  {
+    title: "Apple",
+    demo: "https://majestic-gelato-78453c.netlify.app/",
+    github: "https://github.com/Lawdshev/Apple",
+    img: apple,
+  },
+  {
+    title: "Alicia University",
+    demo: "https://aliciauniversity.netlify.app",
+    github: "https://github.com/Lawdshev/Alicia-University",
+    img: university,
+  },
+  {
+    title: "Vividioz",
+    demo: "https://vividioz.onrender.com/",
+    github: "https://github.com/rash1234567/NEWVIVIDIO",
+    img: vividioz,
+  },
+  {
+    title: "Adserve",
+    demo: "https://adserve.netlify.app/",
+    github: "https://github.com/Lawdshev/adserve",
+    img: adserve,
+  },
+  {
+    title: "AppraiseMe",
+    demo: "https://appraiseMe.netlify.app/",
+    github: "https://github.com/Lawdshev/appraise",
+    img: appraiseMe,
+  },
+  {
+    title: "CountriesQQ",
+    demo: "https://countriesQQ.onrender.com",
+    github: "https://github.com/Lawdshev/vatebra_test",
+    img: country,
+  },
+  {
+    title: "unshelled",
+    demo: "https://unshelled-client.onrender.com/",
+    github: "https://github.com/Lawdshev/unshelled-frontend",
+    img: unshelled,
+  },
+];
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,60 +1,85 @@
 import React from 'react';
 import "./portfolio.css";
 import {Link} from 'react-router-dom';
-import vividioz from '../../Projects/portfolio_img/vivioz.PNG';
-import apple from '../../Projects/portfolio_img/AppleStore.png';
+import streetFood from "../../assets/streetfood.jpeg";
 import university from '../../assets/examinator.jpeg'
 import hq from '../../Projects/portfolio_img/moneyHq.jpeg'
 import wardboard from '../../assets/wardboard.jpg'
 
 const portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
             <img src={hq} alt="" />
           </div>
           <h3>MoneyHQ</h3>
-          <div className='portfolio__item-cta flex justify-around'>
-          <a href="#" className='btn' target='_blank'>Github</a>
-          <a href="https://dev.moneyhq.io/" className='btn btn-primary' target='_blank'>Live
-           Demo</a>
+          <div className="portfolio__item-cta flex justify-around">
+            <a href="#" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://dev.moneyhq.io/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-          <img src={wardboard} alt="" />
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={wardboard} alt="" />
           </div>
           <h3>Wardbord</h3>
-          <div className='portfolio__item-cta flex justify-around'>
-          <a href="#" className='btn' target='_blank'>Github</a>
-          <a href="https://coach.wardbord.com/" className='btn btn-primary' target='_blank'>Live
-           Demo</a>
+          <div className="portfolio__item-cta flex justify-around">
+            <a href="#" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://coach.wardbord.com/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-          <img src={university} alt="" />
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={streetFood} alt="" />
           </div>
-          <h3>Examinator</h3>
-          <div className='portfolio__item-cta flex justify-around'>
-          <a href="#" className='btn' target='_blank'>Github</a>
-          <a href="https://examinator.netlify.app/" className='btn btn-primary' target='_blank'>Live
-           Demo</a>
+          <h3>Street Food</h3>
+          <div className="portfolio__item-cta flex justify-around">
+            <a href="#" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://street-food-eight.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
           </div>
         </article>
       </div>
-      <div className="w-full flex mt-2"> <Link to='/projects' className='mx-auto text-xl md:text-2xl'>See more frontend projects <i class="fa-solid fa-right-long"></i></Link></div>
-      <div className='container portfolio__container'>
+      <div className="w-full flex mt-2">
+        {" "}
+        <Link to="/projects" className="mx-auto text-xl md:text-2xl">
+          See more <i class="fa-solid fa-right-long"></i>
+        </Link>
+      </div>
+      <div className="container portfolio__container">
         {/* <div className="w-full flex mt-2"> <Link to='/backend-projects' className='mx-auto text-xl md:text-2xl'>See more backend projects <i class="fa-solid fa-right-long"></i></Link></div> */}
       </div>
     </section>
-  )
+  );
 }
 export default portfolio
